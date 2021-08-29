9e71b50de856e84e4b0f3536b51e25a11cfa9651
francisco torres <ftorres767@gmail.com>
2021-08-29 15:35:45 -0300

    29/08

--- api/src/routes/Countries.js
+++ api/src/routes/Countries.js
@@ -5,32 +5,41 @@ const { Country , Activity } = require('../db.js');
 const axios = require('axios')
 const { v4: uuidv4 } = require('uuid');
 
-let llamada = async() => {
-    let apiCountries = await axios.get('https://restcountries.eu/rest/v2/all')
-  apiCountriesMap = apiCountries.data.map(country =>{
-    return{
-      id: country.alpha3Code,
-      name:country.name,
-      image:country.flag,
-      continent:country.region,
-      capital:country.capital,
-      area:country.area,
-      population:country.population
-    }
-  })
-  let mibase = await Country.bulkCreate(apiCountriesMap) 
-}
-
 
 router.get("/", async(req, res) => {
-    const myCountries = await Country.findAll() // trae todo lo de la tabla diet
-    console.log(myCountries)
-    if(myCountries.length > 1){ // porque me trae el arreglo pero esta vacio entonces le pongo length
+    const myCountries = await Country.findAll() 
+    if(myCountries.length > 1){ 
         return res.json(myCountries).status(200);
     }else{
-        llamada()
-        return res.json(await Country.findAll()).status(200);
+        let apiCountries = await axios.get('https://restcountries.eu/rest/v2/all')
+        apiCountriesMap = apiCountries.data.map(country =>{
+          return{
+            id: country.alpha3Code,
+            name:country.name,
+            image:country.flag,
+            continent:country.region,
+            capital:country.capital,
+            area:country.area,
+            population:country.population
+          }
+        })
+        await Country.bulkCreate(apiCountriesMap) 
+        const myCountriesCreate = await Country.findAll()
+        return res.json(myCountriesCreate).status(200);
     }
+})
+
+router.get("/:idPais", async (req, res) => {
+    let { idPais } = req.params
+    let traigo = await Country.findAll()
+    console.log(traigo)
+    const myId = await Country.findOne({ where: { id: idPais } });
+    console.log(myId)
+if (myId === null) {
+  res.send('Not found!');
+} else {
+  res.json(myId)
+}
 })
 
 module.exports = router;
